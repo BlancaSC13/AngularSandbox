e1566e1f5bc6f9e44aca7d9713d3c935fe55ad30
BlancaSC13 <blanca.segura@ucr.ac.cr>
2025-01-27 16:49:31 -0600

    Lab angular

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { PlaneComponent } from './app/plane/plane.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(PlaneComponent, appConfig).catch((err) =>
+  console.error(err)
+);
